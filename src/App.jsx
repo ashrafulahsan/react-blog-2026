a31699276bc7857691d8603b9206eca4470b118b
ashrafulahsan <ahsanashraful@email.com>
2026-06-19 17:16:00 +0600

    Vite and React - Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Header from './Header.jsx'
+
+function App() {
+
+  return (
+     <h1>Hello, React!</h1>
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header>
+            <h1>My React Blog Header My React Blog Header My React Blog Header My React Blog Header</h1>
+        </header>
+    );
+}
+
+export default Header;
