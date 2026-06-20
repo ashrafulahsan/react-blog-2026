34cf3ff25278968ea0bf7999aa1564ec1f8352b8
ashrafulahsan <ahsanashraful@email.com>
2026-06-20 22:08:19 +0600

    Data pass and receive way 3

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import User from "./User"
 
 function App() {
 
+  let name="Ashraful"
+  let age=36
+  let email="ahsanashraful@gmail.com"
+
   return (
     <>
-     <User name="Ashraful" age={36}/>
+     <User name={name} age={age} email={email} />
     </>
   )
 }
--- src/User.jsx
+++ src/User.jsx
@@ -1,9 +1,10 @@
-function User({name, age}){
+function User({name, age, email}){
     return(
         <div>
             <h1>User Component</h1>
             <p>Name: {name}</p>
             <p>Age: {age}</p>
+            <p>Email: {email}</p>
         </div>
     )
 }
