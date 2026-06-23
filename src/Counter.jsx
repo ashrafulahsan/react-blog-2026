95d66be98618520a323b506fbad12601f5f946d0
ashrafulahsan <ahsanashraful@email.com>
2026-06-23 19:58:48 +0600

    Use Effect Hook for Life Cycle Methods

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -10,12 +10,30 @@ const Counter=({count, data})=>{
     }
 
     useEffect(()=>{
+        // This code will run for mounting only
+        // Mount: when first time load a code/module
+        console.log('This code will run for mounting only')
         handleCounter()
     }, [])
 
     useEffect(()=>{
+        // This code will call when mounting and update any info
+        console.log('This code will call when mounting and update any info')
         handleData()
     })
+
+    useEffect(()=>{
+        // This code will call when mounting and update data
+        console.log('This code will call when mounting and update data')
+        handleData()
+    }, [data])
+
+    useEffect(()=>{        
+        return()=>{  
+            // This code will run when unmount a component
+            console.log('Call when unmount')        
+        }    
+    }, [])
 
     return(
         <div>
--- src/UseEffectProps.jsx
+++ src/UseEffectProps.jsx
@@ -4,12 +4,16 @@ import Counter from "./Counter"
 function UseEffectProps(){
     const [count,setCount]=useState(0)
     const [data,setData]=useState(0)
+    const [display,setDisplay]=useState(true)
 
     return(
         <div>
-            <Counter count={count} data={data}/>
+            {
+                display?<Counter count={count} data={data}/>:null
+            }    
             <button onClick={()=>setCount(count+1)}>Counter {count}</button>
             <button onClick={()=>setData(data+1)}>Data {data}</button>
+            <button onClick={()=>setDisplay(!display)}>Toggle</button>
         </div>
     )
 }
