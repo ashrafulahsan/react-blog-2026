32f984a8b53fc2d8bf12cbac33343aa3fc325357
ashrafulahsan <ahsanashraful@email.com>
2026-06-23 13:17:31 +0600

    UseEffect: Side effect example

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import CollegeData from "./CollegeData"
+import { useState } from "react";
 
 function App() {
+
+  const [counter,setCounter]=useState(0)
+  const [data,setData]=useState(0)
+
+  function callOnce(){
+    console.log("Call this function once.");
+  }
+
+  callOnce()
+
   return (
     <>
-     <CollegeData />
+      <h1>useEffect Hook</h1> 
+      <button onClick={()=>setCounter(counter+1)}>Counter {counter}</button>
+      <button onClick={()=>setData(data+1)}>Data {data}</button>
     </>
   )
 }
